acebdd6e6e1f4c76e9b59764235e08b98c969881
Sandip Patil <sanppatil@gmail.com>
2021-10-12 01:26:46 -0400

    Added comments

--- src/main/java/org/enodation/exercise3/java8/Main.java
+++ src/main/java/org/enodation/exercise3/java8/Main.java
@@ -3,6 +3,7 @@ package org.enodation.exercise3.java8;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.enodation.exercise3.Person;
 
@@ -13,32 +14,30 @@ public class Main {
 		List<Person> people = Arrays.asList(new Person("John", "Doe", 45), new Person("Ron", "Smith", 32),
 				new Person("Tom", "Hank", 23), new Person("Anglina", "Jolie", 45), new Person("Jack", "Sparrow", 35));
 
-		// Sort list by Last Name
+		// Sort list by Last Name (Using functional interface - Comparator)
 		Collections.sort(people, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
 
-		// Print all people in list
+		// Print all people in list (Using functional interface - Predicate)
 		System.out.println("Printing all persons....");
 		printConditionally(people, (p) -> true);
 
-		// Print all people in list where LastName begins with "S"
+		// Print all people in list where LastName begins with "S" (Using functional interface - Predicate)
 		System.out.println("Printing all persons where LastName begins with 'S'....");
 		printConditionally(people, (p) -> p.getLastName().startsWith("S"));
 
-		// Print all people in list where FirstName begins with "J"
+		// Print all people in list where FirstName begins with "J" (Using functional interface - Predicate)
 		System.out.println("Printing all persons where FirstName begins with 'J'....");
 		printConditionally(people, (p) -> p.getFirstName().startsWith("J"));
 
 	}
 
-	private static void printConditionally(List<Person> people, Condition condition) {
+	private static void printConditionally(List<Person> people, Predicate<Person> predicate) {
 		for (Person person : people) {
-			if (condition.test(person))
+			if (predicate.test(person))
 				System.out.println(person.toString());
 		}
 	}
 
-	interface Condition {
-		boolean test(Person person);
-	}
+
 
 }
